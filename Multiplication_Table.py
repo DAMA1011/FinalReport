e9c2f8f8aa5328c8692b892d17c7f5a6e885fd30
DAMA1011 <oldguy911011@gmail.com>
2022-11-02 08:55:29 +0800

    2022/11/02

--- Multiplication_Table.py
+++ Multiplication_Table.py
@@ -1,12 +1,13 @@
-from concurrent.futures import ThreadPoolExecutor as tpe
+from concurrent.futures import ProcessPoolExecutor as tpe
 import time
 
 def Multi(num , count):
     if count < 10:
         print(f'{num} * {count} = {num*count}', end="\t")
         Multi(num, count + 1)
-        time.sleep(0.01)
+        time.sleep(0.1)
     print()
 
-with tpe(max_workers=9) as executor:
-    executor.map(Multi, [1, 2, 3, 4, 5, 6, 7, 8, 9], [1] * 9)
+if __name__ == "__main__":
+    with tpe(max_workers=9) as executor:
+        executor.map(Multi, [1, 2, 3, 4, 5, 6, 7, 8, 9], [1] * 9)
