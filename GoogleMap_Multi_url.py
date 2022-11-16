8d87b94c0465e1b4c76bdb00ec571b09f3e61bea
DAMA1011 <oldguy911011@gmail.com>
2022-11-16 17:57:19 +0800

    2022/11/16 school

--- GoogleMap_Multi_url.py
+++ GoogleMap_Multi_url.py
@@ -52,6 +52,7 @@ def browser():
     my_options.add_argument("--lang=zh-TW")  #設定為正體中文
     my_options.add_argument('--disable-gpu')  # google document 提到需要加上這個屬性來規避 bug
     my_options.add_experimental_option("detach", True)
+    my_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     my_service = Service(ChromeDriverManager().install())
 
     # 使用 Chrome 的 WebDriver
@@ -83,7 +84,7 @@ def TargetMap(links: str):
     ac.send_keys(Keys.ENTER)
     ac.perform()
 
-    sleep(1) # 依網路狀況調整
+    sleep(4) # 依網路狀況調整
 
     urlList = []  # 存放首頁查詢滾 動完的所有店家資訊網址
 
@@ -94,67 +95,80 @@ def TargetMap(links: str):
     limit = 2  # 最大無效滾動次數
     done = True 
 
-    # 持續捲動
-    while done:
+    try:
 
-        # 等待篩選元素出現
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, 'div[role="feed"]')
-            )
-        )
+        # # 等待篩選元素出現
+        # WebDriverWait(driver, 10).until(
+        #     EC.presence_of_element_located(
+        #         (By.CSS_SELECTOR, 'div[role="feed"]')
+        #     )
+        # )
 
         # focus: 主角頁面
         focus = driver.find_element(By.CSS_SELECTOR, 'div[role="feed"]')
+        pass
 
-        try:
+        # 持續捲動
+        while done:
+
+            try:
+
+                # focus: 主角頁面
+                focus = driver.find_element(By.CSS_SELECTOR, 'div[role="feed"]')
+
+                # 檢查是否滾動到底，並且有顯示「你已看完所有搜尋結果」的標籤
+                driver.find_element(By.CSS_SELECTOR, 'div[data-js-log-root] div[data-js-log-root] div[data-js-log-root] p[style^="font-family"] span[style^="color"]')
+
+                break
+                
+            except NoSuchElementException:
+
+                # offset: 拉槓到頁面頂端的距離
+                offset = driver.execute_script('return arguments[0].scrollTop', focus)
+                # print(offset)
+
+                # 執行js指令捲動頁面
+                driver.execute_script('arguments[0].scrollTo(0, arguments[0].scrollHeight)', focus)
+
+                # innerHeight: 頁面高度 = 拉槓到頁面頂端的距離
+                innerHeight = driver.execute_script('return arguments[0].scrollHeight = arguments[0].scrollTop', focus)
+                # print(innerHeight)
+
+                sleep(2)  # 依網路狀況調整
+
+                # 經過計算，如果「拉槓到頁面頂端的距離」(offset)等於「頁面高度 = 拉槓到頁面頂端的距離」(innerHeight)，代表已經到底了
+                if offset == innerHeight:
+                    count += 1
+
+                # 計數器等於限制數則跳脫
+                if count == limit:
+
+                    print(f'[{links}] 捲動失敗! 重新整理!')
+
+                    count = 0  # 計數器歸零
+
+                    driver.refresh()  # 重整頁面
             
-            # 檢查是否滾動到底，並且有顯示「你已看完所有搜尋結果」的標籤
-            driver.find_element(By.CSS_SELECTOR, 'div[data-js-log-root] div[data-js-log-root] div[data-js-log-root] p[style^="font-family"] span[style^="color"]')
+        # 紀錄首頁滾動完的所有店家資訊網址
+        for a in driver.find_elements(By.CSS_SELECTOR, 'div[data-js-log-root] div[role="article"] a[aria-label]'):
+            urlList.append(a.get_attribute("href"))
 
-            break
-            
-        except NoSuchElementException:
+        print(f'[{links}] 搜尋已到底，滾動結束，有 {len(urlList)} 筆店家')
 
-            # offset: 拉槓到頁面頂端的距離
-            offset = driver.execute_script('return arguments[0].scrollTop', focus)
-            # print(offset)
+        sleep(2)
 
-            # 執行js指令捲動頁面
-            driver.execute_script('arguments[0].scrollTo(0, arguments[0].scrollHeight)', focus)
+        driver.quit()
+        
+        # pprint.pprint(urlList)
+        # print(len(urlList))
 
-            # innerHeight: 頁面高度 = 拉槓到頁面頂端的距離
-            innerHeight = driver.execute_script('return arguments[0].scrollHeight = arguments[0].scrollTop', focus)
-            # print(innerHeight)
+        return urlList
 
-            sleep(2)  # 依網路狀況調整
+    except NoSuchElementException:
 
-            # 經過計算，如果「拉槓到頁面頂端的距離」(offset)等於「頁面高度 = 拉槓到頁面頂端的距離」(innerHeight)，代表已經到底了
-            if offset == innerHeight:
-                count += 1
+        driver.quit()
 
-            # 計數器等於限制數則跳脫
-            if count == limit:
-
-                print(f'[{links}] 捲動失敗! 重新整理!')
-
-                count = 0  # 計數器歸零
-
-                driver.refresh()  # 重整頁面
-            
-    # 紀錄首頁滾動完的所有店家資訊網址
-    for a in driver.find_elements(By.CSS_SELECTOR, 'div[data-js-log-root] div[role="article"] a[aria-label]'):
-        urlList.append(a.get_attribute("href"))
-
-    print(f'[{links}] 搜尋已到底，滾動結束，有 {len(urlList)} 筆店家')
-
-    sleep(2)
-
-    driver.quit()
-       
-    # pprint.pprint(urlList)
-
-    return urlList
+        return []
 
 # 平行處理
 def FirstPage():
@@ -165,7 +179,7 @@ def FirstPage():
 
     street = ['大同區大龍街']
 
-    category = ['火鍋', '拉麵', '日式', '美式', '義式', '法式', '中式', '台式', '韓式', '德式', '地中海料理', '印度料理', '越式']
+    category = ['鐵板燒', '牛排', '火鍋', '拉麵', '日本料理', '美式', '義式', '法式', '中式', '台灣菜', '韓式', '德式', '地中海料理', '印度料理', '越式', '港式', '泰式', '南洋', '素食', '餐酒館', '咖啡廳', '熱炒店', '早午餐', '甜點店', '燒肉', '海鮮餐廳']
 
     for item_1 in street:
         for item_2 in category:
@@ -173,7 +187,7 @@ def FirstPage():
 
             keywords.append(links) 
 
-    with ppe(max_workers=3) as executor:     
+    with ppe(max_workers=8) as executor:     
         results = [executor.submit(TargetMap, key) for key in keywords]
         try:
             for result in as_completed(results):
@@ -182,7 +196,7 @@ def FirstPage():
             pass
 
     allurlList.append({
-        "herf": list(set(comList))  # 篩選掉重複的網址
+        "herf": comList  # 篩選掉重複的網址
     })
 
     # # pprint.pprint(allurlList)
@@ -194,13 +208,14 @@ def FirstPage():
     
     sleep(3)
 
+    print(len(comList))
+
 if __name__ == '__main__':
     time1 = time.time()
     FirstPage()
     print(f'執行總花費時間: {time.time() - time1}')
 
 
-# ['大同區大龍街餐廳', '大同區五原路餐廳', '大同區天水路餐廳', '大同區太原路餐廳', '大同區市民大道一段餐廳', '大同區平陽街餐廳', '大同區民生西路餐廳', '大同區民族西路餐廳', '大同區民樂街餐廳', '大同區民權西路餐廳', '大同區永昌街餐廳', '大同區甘州街餐廳', '大同區甘谷街餐廳', '大同區伊寧街餐廳', '大同區安西街餐廳', '大同區西寧北路餐廳', '大同區赤峰街餐廳', '大同區延平北路一段餐廳', '大同區延平北路二段餐廳', '大同區延平北路三段餐廳', '大同區延平北路四段餐廳', '大同區忠孝西路二段餐廳', '大同區承德路一段餐廳', '大同區承德路二段餐廳', '大同區承德路三段餐廳', '大同區昌吉街餐廳', '大同區長安西路餐廳', '大同區保安街餐廳', '大同區南京西路餐廳', '大同區哈密街餐廳', '大同區迪化街一段餐廳', '大同區迪化街二段餐廳', '大同區重慶北路一段餐廳', '大同區重慶北路二段餐廳', '大同區重慶北路三段餐廳', '大同區庫倫街餐廳', '大同區酒泉街餐廳', '大同區涼州街餐廳', '大同區通河西街一段餐廳', '大同區敦煌路餐廳', '大同區景化街餐廳', '大同區華亭街餐廳', '大同區華陰街餐廳', '大同區貴德街餐廳', '大同區塔城街餐廳', '大同區萬全街餐廳', '大同區寧夏路餐廳', '大同區撫順街餐廳', '大同區鄭州路餐廳', '大同區興城街餐廳', '大同區錦西街餐廳', '大同區環河北路一段餐廳', '大同區環河北路二段餐廳', '大同區歸綏街餐廳', '大同區雙連街餐廳', '大同區蘭州街餐廳']
+# ['大同區大龍街', '大同區五原路', '大同區天水路', '大同區太原路', '大同區市民大道一段', '大同區平陽街', '大同區民生西路', '大同區民族西路', '大同區民樂街', '大同區民權西路', '大同區永昌街', '大同區甘州街', '大同區甘谷街', '大同區伊寧街', '大同區安西街', '大同區西寧北路', '大同區赤峰街', '大同區延平北路一段', '大同區延平北路二段', '大同區延平北路三段', '大同區延平北路四段', '大同區忠孝西路二段', '大同區承德路一段', '大同區承德路二段', '大同區承德路三段', '大同區昌吉街', '大同區長安西路', '大同區保安街', '大同區南京西路', '大同區哈密街', '大同區迪化街一段', '大同區迪化街二段', '大同區重慶北路一段', '大同區重慶北路二段', '大同區重慶北路三段', '大同區庫倫街', '大同區酒泉街', '大同區涼州街', '大同區通河西街一段', '大同區敦煌路', '大同區景化街', '大同區華亭街', '大同區華陰街', '大同區貴德街', '大同區塔城街', '大同區萬全街', '大同區寧夏路', '大同區撫順街', '大同區鄭州路', '大同區興城街', '大同區錦西街', '大同區環河北路一段', '大同區環河北路二段', '大同區歸綏街', '大同區雙連街', '大同區蘭州街']
 
-# ['火鍋', '拉麵', '日式', '美式', '義式', '法式', '中式', '台式', '韓式', '德式', '地中海料理', '印度料理', '越式'] # 26 * category
-# [ '港式', '泰式', '南洋', '素食', '鐵板燒', '餐酒館', '咖啡廳', '熱炒店', '早午餐', '甜點店', '燒肉', '海鮮餐廳', '牛排']
+# ['火鍋', '拉麵', '日本料理', '美式', '義式', '法式', '中式', '台灣菜', '韓式', '德式', '地中海料理', '印度料理', '越式', '港式', '泰式', '南洋', '素食', '鐵板燒', '餐酒館', '咖啡廳', '熱炒店', '早午餐', '甜點店', '燒肉', '海鮮餐廳', '牛排'] # 26 * category
